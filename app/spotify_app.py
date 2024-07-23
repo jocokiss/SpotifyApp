73ee00842fe7f9d82b1e0855e3e97da341b7da0c
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2024-07-23 12:28:30 +0200

    Finished with genre update on tracks

--- app/spotify_app.py
+++ app/spotify_app.py
@@ -66,14 +66,14 @@ class SpotifyApp(BaseModel):
         ]
         return pd.DataFrame(dict_list)
 
-    def __initialize_user_connection(self):
+    def __initialize_user_connection(self) -> spotipy.Spotify:
         return spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id=self.__client_id,
             client_secret=self.__client_secret,
             scope="user-library-read",
             redirect_uri="https://ca2f-89-135-32-37.ngrok-free.app"))
 
-    def __initialize_client_connection(self):
+    def __initialize_client_connection(self) -> spotipy.Spotify:
         client_credentials_manager = SpotifyClientCredentials(
             client_id=self.__client_id, client_secret=self.__client_secret)
 
@@ -87,15 +87,42 @@ class SpotifyApp(BaseModel):
             tracks.extend(results["items"])
         return [Tracks(**track['track']) for track in tracks]
 
-    def get_genres(self, tracklist: list[Tracks]) -> pd.DataFrame:
-        artists = {artist['name'] for track in tracklist for artist in track.artists}
+    def update_tracks_with_genres(self, tracklist: list[Tracks]) ->pd.DataFrame:
+        tracklist_df = self.get_dataframe_from_tracks(tracklist)
+        genre_df = self.__get_genres_by_artists(tracklist)
+
+        tracklist_df['artists'] = tracklist_df['artists'].apply(lambda x: [artist.strip().lower() for artist in x])
+        tracklist_exploded = tracklist_df.explode('artists')
+
+        df_tracks_merged = pd.merge(tracklist_exploded, genre_df, left_on='artists', right_on='artist', how='left')
+        df_tracks_merged['genre'] = df_tracks_merged['genre'].apply(lambda x: x if isinstance(x, list) else [])
+
+        df_grouped = df_tracks_merged.groupby(['name', 'uri']).agg({
+            'album': 'first',
+            'explicit': 'first',
+            'popularity': 'first',
+            'id': 'first',
+            'artists': lambda x: list(set(x)),
+            'genre': lambda x: list(set([genre for sublist in x for genre in sublist]))
+        }).reset_index(drop=False)
+
+        return df_grouped
+
+    def __get_genres_by_artists(self, tracklist: list[Tracks]) -> pd.DataFrame:
+        artists = {artist['name'].strip().lower() for track in tracklist for artist in track.artists}
         df_dict = {}
+
         for artist in artists:
-            query = f"artists:{artist}"
-            search_results = self.__client_conn.search(q=query, type="artist", limit=50)["artists"]["items"]
-            for result in search_results:
-                if result['name'] == artist:
-                    df_dict.update({artist: result["genres"]})
+            query = f"artist:{artist}"
+            try:
+                search_results = self.__client_conn.search(q=query, type="artist", limit=50)["artists"]["items"]
+                for result in search_results:
+                    if result['name'].strip().lower() == artist:
+                        df_dict[artist] = result["genres"]
+                        break
+            except Exception as e:
+                print(f"Error fetching genres for artist {artist}: {e}")
+                df_dict[artist] = []
 
         normalized_data = [(artist, genre) for artist, genres in df_dict.items() for genre in genres]
         df = pd.DataFrame(normalized_data, columns=['artist', 'genre'])
@@ -129,7 +156,7 @@ class SpotifyApp(BaseModel):
         """Get audio features from tracks.
 
         Args:
-            tracks: Provide either a list of tracks or a path to a json file containing tracks.
+            tracklist: Provide either a list of tracks or a path to a json file containing tracks.
 
         Returns: list of audio features.
 
@@ -154,58 +181,8 @@ class SpotifyApp(BaseModel):
 SP = SpotifyApp()
 queried_tracks = SP.search_tracks(query="year:2022-2023 genre:pop", limit=50, market='US')
 
-user_songs_df = SP.get_dataframe_from_tracks(queried_tracks)
-genre_df = SP.get_genres(queried_tracks)
+pd.set_option('display.max_rows', None)  # Show all rows
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.width', None)
 
-pd.set_option('display.max_columns', None)
-
-# Now printing the dataframe will show all columns
-
-
-df_tracks_exploded = user_songs_df.explode('artists')
-
-df_tracks_merged = pd.merge(df_tracks_exploded, genre_df, left_on='artists', right_on='artist', how='left')
-
-print(df_tracks_merged)
-
-df_tracks_merged['genre'] = df_tracks_merged['genre'].apply(lambda x: x if isinstance(x, list) else [])
-
-
-df_result = (
-    df_tracks_merged
-    .groupby(['name', 'album', 'explicit', 'uri', 'popularity', 'id'])
-    .agg({
-        'artists': lambda x: list(set(x)),  # Aggregating unique artists
-        'genre': lambda x: list(set(genre for sublist in x for genre in sublist))  # Flattening and deduplicating genres
-    })
-    .reset_index()
-)
-
-# Filter rows where 'genre' is an empty list
-empty_genres_df = df_result[df_result['genre'].apply(lambda x: x == [])]
-
-# Count the number of such rows
-num_empty_genres = len(empty_genres_df)
-
-# liked_features = SP.get_audio_features("audio_features.json")
-
-# X_liked = SP.convert_to_numpy_array(features)
-# Y_liked = SP.convert_to_numpy_array(liked_features)
-# average_features = np.mean(X_liked, axis=0)
-
-# df = SP.get_user_playlist_dataframe
-# # Normalize the nested JSON in the 'track' column
-# track_df = pd.json_normalize(df['track'])
-# 
-# # Extract and rename the required columns
-# track_df = track_df[['album.name', 'artists', 'explicit', 'name', 'id']]
-# track_df.columns = ['album', 'artist_list', 'explicit', 'name', 'id']
-# 
-# # Since 'artist_list' is a list of dictionaries, extract the artist names
-# track_df['artist'] = track_df['artist_list'].apply(lambda x: ', '.join([artist['name'] for artist in x]))
-# 
-# # Drop the original 'artist_list' column
-# track_df = track_df.drop(columns=['artist_list'])
-# 
-# # Concatenate the 'added_at' column
-# final_df = pd.concat([df[['added_at']], track_df], axis=1)
+print(SP.update_tracks_with_genres(queried_tracks))
